2e0ca2bf911e644966968cddac5cead4d19bbc0a
Josep <josep.romera@enti.cat>
2024-09-08 23:09:06 +0200

    Movement animation

--- Assets/Project/Controls/InputManager.cs
+++ Assets/Project/Controls/InputManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
     PlayerInput playerControls;
+    AnimationManager animationManager;
 
     public Vector2 movementInput;
+    private float moveAmount;
+
     public float verticalInput;
     public float horizontalInput;
+
+    private void Awake()
+    {
+        animationManager = GetComponent<AnimationManager>();
+    }
     private void OnEnable()
     {
         if(playerControls == null)
@@ -33,5 +42,7 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput)) + Mathf.Abs(verticalInput);
+        animationManager.UpdateAnimationValues(0, moveAmount);
     }
 }
--- Assets/Project/Scripts/Player/PlayerManager.cs
+++ Assets/Project/Scripts/Player/PlayerManager.cs
@@ -22,4 +22,6 @@ public class PlayerManager : MonoBehaviour
     {
         playerLocomotion.HandleAllMovement();
     }
+
+
 }
