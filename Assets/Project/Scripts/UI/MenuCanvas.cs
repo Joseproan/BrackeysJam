a208f7e39cc1e7d713d99b734cb6ec97e26db6b1
Josep <josep.romera@enti.cat>
2024-09-13 19:17:31 +0200

    Main Menu

--- Assets/Project/Scripts/UI/MenuCanvas.cs
+++ Assets/Project/Scripts/UI/MenuCanvas.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 
 public class MenuCanvas : MonoBehaviour
 {
+    [SerializeField] private GameObject menuItems;
+    [SerializeField] private GameObject crediPanel;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject backButtonCredits;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,8 +30,16 @@ public class MenuCanvas : MonoBehaviour
 
     public void Credits()
     {
-
+        crediPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(backButtonCredits);
     }
+
+    public void BackButtonCredits()
+    {
+        crediPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(playButton);
+    }
+
     public void ExitGame()
     {
         ExitGame();
